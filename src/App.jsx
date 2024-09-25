6220706e4baa12a6104b73d43ce40a06adf6940c
shayan407 <152868142+shayan407@users.noreply.github.com>
2024-09-25 07:13:03 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [todo, settodo] = useState("");
